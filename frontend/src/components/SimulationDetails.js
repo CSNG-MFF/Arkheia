0895b55c55ed395e721c9f67efb828783e87f77f
Norbert Horvath <nhorvathkzgimi@gmail.com>
2024-01-06 21:25:42 +0100

    trying out multiple json viewers

--- frontend/src/components/SimulationDetails.js
+++ frontend/src/components/SimulationDetails.js
@@ -1,17 +1,53 @@
 import { IoTrashSharp } from "react-icons/io5";
 import { IoMdDownload } from "react-icons/io";
+
 import React, { useState, useRef } from 'react'
 
-import { Button, Alert } from "reactstrap";
+import { Button, Alert, Popover, PopoverBody } from "reactstrap";
 
 const SimulationDetail = ({ simulation }) => {
   const [alertDeleteVisible, setAlertDeleteVisible] = useState(false);
+  const [popoverOpen, setPopoverOpen] = useState(false);
 
+  const togglePopover = () => {
+    setPopoverOpen(!popoverOpen);
+  };
+
+  const data = {
+      "name": "John Doe",
+      "age": 30,
+      "city": "New York",
+      "isStudent": false,
+      "hobbies": ["reading", "traveling", "coding"],
+      "address": {
+        "street": "123 Main St",
+        "zipCode": "10001",
+        "country": "USA"
+      },
+      "friends": [
+        {
+          "name": "Jane Smith",
+          "age": 28,
+          "isStudent": true
+        },
+        {
+          "name": "Bob Johnson",
+          "age": 32,
+          "isStudent": false
+        }
+      ]
+    }
+  
+  
   const handleDelete = async () => {
     const response = await fetch('/simulation_runs/' + simulation._id, {
       method: 'DELETE'
     })
     const json = await response.json()
+
+    if (!response.ok) {
+      console.error(json.error);
+    }
 
     if (response.ok) {
       console.log('simulation deleted');
@@ -41,7 +77,17 @@ const SimulationDetail = ({ simulation }) => {
         <td>
           {simulation.model_name}
         </td>
-        <td>a</td>
+        <td>
+          <Button id="popoverButton" type="button">
+            Open Popover
+          </Button>
+          <Popover placement="bottom" isOpen={popoverOpen} target="popoverButton" toggle={togglePopover}>
+            <PopoverBody>
+              This is the content of my popover.
+              
+            </PopoverBody>
+          </Popover>
+        </td>
         <td>a</td>
         <td>a</td>
         <td>a</td>
