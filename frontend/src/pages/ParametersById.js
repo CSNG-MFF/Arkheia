04e569f3fd0dc34dd4bf4f90bde9d5e34b32804c
Norbert Horvath <nhorvathkzgimi@gmail.com>
2024-03-04 14:26:12 +0100

    finalised simulation adding

--- frontend/src/pages/ParametersById.js
+++ frontend/src/pages/ParametersById.js
@@ -3,6 +3,27 @@ import { JSONTree } from 'react-json-tree';
 
 import { useParams } from 'react-router-dom';
 
+const theme = {
+  scheme: 'monokai',
+  author: 'wimer hazenberg (http://www.monokai.nl)',
+  base00: '#272822',
+  base01: '#383830',
+  base02: '#49483e',
+  base03: '#75715e',
+  base04: '#a59f85',
+  base05: '#f8f8f2',
+  base06: '#f5f4f1',
+  base07: '#f9f8f5',
+  base08: '#f92672',
+  base09: '#fd971f',
+  base0A: '#f4bf75',
+  base0B: '#a6e22e',
+  base0C: '#a1efe4',
+  base0D: '#66d9ef',
+  base0E: '#ae81ff',
+  base0F: '#cc6633',
+};
+
 const ParametersById = () => {
   const { id } = useParams();
   const [parameters, setParameters] = useState(null);
@@ -27,9 +48,14 @@ const ParametersById = () => {
 
   return (
     <div>
-      <h2>Parameters of the simulation</h2>
+      <h1 style={{ textAlign: 'center'}}>Parameters of the simulation</h1>
       {parameters ? (
-        <JSONTree data={parameters} />
+        <JSONTree 
+        hideRoot={true}
+        labelRenderer={([key]) => <strong>{key}</strong>}
+        invertTheme={true}
+        theme={theme}
+        data={parameters} />
       ) : (
         <p>Loading...</p>
       )}
