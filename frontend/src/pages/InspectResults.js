0bb9c55f09e4d784ce2f7f1285e1040537729e8b
Norbert Horvath <nhorvathkzgimi@gmail.com>
2024-04-20 21:42:02 +0200

    adde parametersearch inspect results--not finished

--- frontend/src/pages/InspectResults.js
+++ frontend/src/pages/InspectResults.js
@@ -1,7 +1,7 @@
 import { useLocation } from 'react-router-dom'
-import { useEffect, useState } from "react"
+import React, { useEffect, useState } from "react"
 
-import { Container, Row, Col, Form, FormGroup, Label, Input } from 'reactstrap'; // Import Reactstrap components
+import { Container, Button, Row, Col, Form, FormGroup, Label, Input, Table } from 'reactstrap'; // Import Reactstrap components
 
 const InspectResults = () => {
   
@@ -10,150 +10,267 @@ const InspectResults = () => {
   
   const [imageScale, setImageScale] = useState(1);
 
-  const [simulations, setSimulations] = useState(null);
+  const [selectedKey, setSelectedKey] = useState(null);
+
+  const [parameterDifferences, setParameterDifferences] = useState({});
+  const [isLoadingSimulations, setIsLoadingSimulations] = useState(false);
+  const [isLoadingResults, setIsLoadingResults] = useState(false);
+  const [simulations, setSimulations] = useState([]);
   const [results, setResults] = useState([]);
 
-  const [differences, setDifferences] = useState([]); 
-  const [selectedResult, setSelectedResult] = useState(null);
-  const [imageSrc, setImageSrc] = useState(null);
+  const [availableResultNames, setAvailableResultNames] = useState([]); // For result name options
+
+  const [selectedResults, setSelectedResults] = useState([]);
+  const [selectedResultName, setSelectedResultName] = useState(''); // For tracking the selected result name
+
+
+  const [selectedKeys, setSelectedKeys] = useState({});
+
 
   useEffect(() => {
-    const fetchSimulations = async () => {
-      if (parameter_search && parameter_search._id) {
-        const response = await fetch(`/parameter_searches/${parameter_search._id}/simulations`);
-        const json = await response.json();
-        if (response.ok) {
-          setSimulations(json);
-        }
-      }
-    }
-    const calculateDifferences = () => {
-      if (simulations) {
-        const allDifferences = [];
-        for (let i = 0; i < simulations.length; i++) {
-          for (let j = i + 1; j < simulations.length; j++) {
-            const diffs = compareParameters(
-              simulations[i].parameters,
-              simulations[j].parameters
-            );
-            allDifferences.push({ 
-              sim1: simulations[i].simulation_run_name, // Or whatever identifies a simulation 
-              sim2: simulations[j].simulation_run_name, 
-              diffs 
-            });
+    if (!simulations.length && !isLoadingSimulations && !results.length && !isLoadingResults) { // Check if simulations are empty and not already loading
+      setIsLoadingSimulations(true);
+      setIsLoadingResults(true);
+
+      const fetchData = async () => {
+        if (parameter_search && parameter_search._id) {
+          const response = await fetch(`/parameter_searches/${parameter_search._id}/simulations`);
+          const json = await response.json();
+          if (response.ok) {
+            setSimulations(json);
+            setIsLoadingSimulations(false);
+            try {
+              const allResults = []; // Array to store results from all simulations
+          
+              for (const simulation of json) {
+                const response = await fetch(`/results/${simulation._id}`);
+                const resultsData = await response.json();
+                setAvailableResultNames(resultsData);
+                if (response.ok) {
+                  allResults.push(...resultsData); // Add results for this simulation
+                } else {
+                  console.error(`Error fetching results for simulation ${simulation._id}:`, response.statusText);
+                }
+              }
+              setResults(allResults);
+              setIsLoadingResults(false);
+    
+              const parameterVariations = {};
+
+            for (const simulation of json) {
+              addVariations(simulation.parameters.sheets, 'sheets', parameterVariations);
+            }
+
+            // Convert all sets to arrays for easier usage later on
+            for (const key in parameterVariations) {
+              parameterVariations[key] = Array.from(parameterVariations[key]);
+              if (parameterVariations[key].length <= 1) {
+                delete parameterVariations[key];
+              }
+            }
+
+            setParameterDifferences(parameterVariations);
+            } catch (error) {
+              console.error('Error fetching results (general):', error);
+            }
           }
         }
-        setDifferences(allDifferences);
       }
-    };
-    const fetchResults = async () => {
-      try {
-        const response = await fetch(`/results/` + simulations[0]._id);
-        const resultsData = await response.json();
-        if (response.ok) {
-          setResults(resultsData);
-        }
-      } catch (error) {
-        console.error('Error fetching results:', error);
+      fetchData();
+    }
+  }, [])
+
+  useEffect(() => {
+    if (parameterDifferences) {
+      setSelectedKeys(
+        Object.keys(parameterDifferences).reduce((acc, key) => {
+          acc[key] = parameterDifferences[key].slice(); // start with all values selected
+          return acc;
+        }, {})
+      );
+      const firstKey = Object.keys(parameterDifferences)[0];
+      if (firstKey) {
+        setSelectedKey(firstKey);
       }
-    };
-    fetchResults();
-    fetchSimulations();
-    calculateDifferences();
-  }, [simulations])
+      const otherKeys = Object.keys(parameterDifferences).filter(key => key !== selectedKey);
+
+      // Generate all combinations of other key values
+      let combinations = otherKeys.flatMap(key => 
+        parameterDifferences[key].map(value => ({ [key]: value }))
+      );
+
+      // Generate the table
+      let table = {};
+      if (parameterDifferences[selectedKey]) {
+        parameterDifferences[selectedKey].forEach(xValue => {
+          table[xValue] = {};
+          combinations.forEach(combination => {
+            // Find a simulation that matches the combination and xValue
+            const matchingSimulation = simulations.find(simulation => 
+              Object.entries(combination).every(([key, value]) => 
+                simulation.parameters.sheets[key] === value
+              ) && simulation.parameters.sheets[selectedKey] === xValue
+            );
+            // If a matching simulation is found, add its name to the table
+            if (matchingSimulation) {
+              table[xValue][JSON.stringify(combination)] = matchingSimulation.simulation_run_name;
+            }
+          });
+        });
+      }     
+      console.log("table: ", table);
+    }
+  }, [parameterDifferences]);
 
   const handleResultChange = (event) => {
+    setSelectedResults([]);
     const selectedName = event.target.value;
-    const selectedResultData = results.find((result) => result.name === selectedName);
-    setSelectedResult(selectedResultData);
+    const matchingResults = results.filter((result) => result.name === selectedName);
+    setSelectedResults([...matchingResults]);
+    setSelectedResultName(selectedName);
   };
 
   const handleScaleChange = (event) => {
-    setImageScale(parseFloat(event.target.value));
+    setImageScale(parseFloat(event.target.value).toFixed(2)); // Limit to 2 decimal places
   };
-  return (
-    <Container> {/* Use Reactstrap's Container for basic layout */}
-          {results.length > 0 && (
-            <div>
-              <h2>Select Result</h2>
-              <Form> {/* Use Reactstrap's Form for layout */}
-                <FormGroup>
-                  <Label for="resultSelect">Result</Label>
-                  <Input
-                    type="select"
-                    id="resultSelect"
-                    value={selectedResult?.name || ''}
-                    onChange={handleResultChange}
-                  >
-                    <option value="">Select a Result</option>
-                    {results.map((result) => (
-                      <option key={result._id} value={result.name}>
-                        {result.name}
-                      </option>
-                    ))}
-                  </Input>
-                </FormGroup>
-              </Form>
+  const toggleSelection = (key, value) => {
+    setSelectedKeys(prevState => {
+      // Create a deep copy of prevState to avoid directly mutating state
+      const newSelectedKeys = JSON.parse(JSON.stringify(prevState));
+  
+      // Check if key exists in newSelectedKeys, if not initialize it
+      if (!newSelectedKeys[key]) {
+        newSelectedKeys[key] = [];
+      }
+  
+      const index = newSelectedKeys[key].indexOf(value);
+  
+      if (index === -1) {
+        newSelectedKeys[key].push(value); // select
+      } else if (newSelectedKeys[key].length > 1) {
+        newSelectedKeys[key].splice(index, 1); // deselect if not the last one
+      }
+  
+      return newSelectedKeys;
+    });
+  };
 
-              {selectedResult && (
-                <div style={{ overflowX: 'auto', whiteSpace: 'nowrap' }}> {/* Scrollable container */}
+  return (
+    <Container> {(isLoadingSimulations || isLoadingResults) ? (
+        <div>Loading...</div>
+      ) : ( 
+        <div>
+        { results.length > 0 && (
+          <div>
+            <h2>Select Result</h2>
+            <Form>
+              <FormGroup>
+                <Label for="resultSelect">Result</Label>
+                <Input
+                  type="select"
+                  id="resultSelect"
+                  value={selectedResultName}
+                  onChange={handleResultChange}
+                >
+                  <option value="">Select a Result</option>
+                  {availableResultNames.map((result) => (
+                    <option key={result._id} value={result.name}>
+                      {result.name}
+                    </option>
+                  ))}
+                </Input>
+              </FormGroup>
+            </Form>
+            <div>
+              <label htmlFor="scaleSlider">Image Scale</label>
+              <input
+                type="range"
+                id="scaleSlider"
+                min="0.1"
+                max="3"  
+                step="0.01"  // Smaller step for smoother transitions
+                value={imageScale}
+                onChange={handleScaleChange}
+                style={{ width: '300px' }} // Or adjust to your desired width
+              />
+            </div>
+            {parameterDifferences && Object.keys(parameterDifferences).length > 0 && (
+              <div>
+                {Object.keys(parameterDifferences).map((key) => (
+                  <div key={key} style={{ display: 'flex', alignItems: 'center', flexWrap: 'wrap' }}>
+                    <Button
+                      style={{ margin: '10px'}}
+                      color={key === selectedKey ? 'primary' : 'secondary'}
+                      onClick={() => setSelectedKey(key)}
+                    >
+                      {console.log(selectedKey)}
+                      {key}
+                    </Button>
+                    {parameterDifferences[key].map((value, index) => {
+                      const isSelected = selectedKeys[key] && selectedKeys[key].includes(value);
+                      const isLastSelected = isSelected && selectedKeys[key].length === 1;
+
+                      return (
+                        <Button 
+                          color={isLastSelected ? 'danger' : isSelected ? 'success' : 'secondary'}
+                          key={index}
+                          style={{ margin: '10px'}}
+                          onClick={() => !isLastSelected && toggleSelection(key, value)}
+                          disabled={isLastSelected}
+                        >
+                          {console.log(selectedKeys)}
+                          {value}
+                        </Button>
+                      );
+                    })}
+
+                  </div>
+                ))}
+              </div>
+            )}
+            
+            {selectedResults && selectedResults.length > 0 && (
+              <div style={{ overflowX: 'auto', whiteSpace: 'nowrap' }}>
+                {selectedResults.map((result) => (
                   <img
-                    src={`/results/${selectedResult._id}/image`} 
+                    key={result._id} // Important: Unique key for each image
+                    src={`/results/${result._id}/image`}
                     alt="Result Figure"
-                    style={{ 
-                      maxWidth: '100%', 
+                    style={{
+                      maxWidth: '100%',
                       height: 'auto',
-                      transform: `scale(${imageScale})`
+                      transform: `scale(${imageScale})`,
+                      marginRight: '10px'
                     }}
                   />
-                </div>
-              )}
-
-              <div>
-                <label htmlFor="scaleSlider">Image Scale</label>
-                <input
-                  type="range"
-                  id="scaleSlider"
-                  min="0.1"
-                  max="3"  
-                  step="0.1"  
-                  value={imageScale}
-                  onChange={handleScaleChange}
-                />
+                ))}
               </div>
-            </div>
-          )}
+            )}
+          </div>
+        )}
+        </div>
+      )}
     </Container>
   );
 };
 
+
+
 export default InspectResults
 
-function compareParameters(parameters1, parameters2, depth = 1) {
-  const differences = [];
-
-  // Ensure both simulations have a 'sheets' object
-  if (parameters1.sheets && parameters2.sheets) {
-    const sheets1 = parameters1.sheets;
-    const sheets2 = parameters2.sheets;
-
-    for (const sheetName in sheets1) {
-      if (!sheets2.hasOwnProperty(sheetName) || !isEqual(sheets1[sheetName], sheets2[sheetName])) {
-          differences.push({ path: `sheets.${sheetName}`, value1: sheets1[sheetName], value2: sheets2[sheetName] });
-      }
-
-      // Recursive comparison for deeper levels (if desired)
-      if (depth > 1 && typeof sheets1[sheetName] === 'object') {
-        const nestedDifferences = compareParameters(sheets1[sheetName], sheets2[sheetName], depth - 1);
-        differences.push(...nestedDifferences.map(diff => ({ path: `sheets.${sheetName}.${diff.path}`, ...diff })));
+function addVariations(obj, path, parameterVariations) {
+  for (const key in obj) {
+    if (typeof obj[key] === 'object' && obj[key] !== null) {
+      // If the value is an object, recursively search it
+      addVariations(obj[key], `${path}.${key}`, parameterVariations);
+    } else {
+      // If the value is not an object, add it to the variations
+      const fullPath = `${path}.${key}`;
+      if (parameterVariations[fullPath]) {
+        parameterVariations[fullPath].add(obj[key]);
+      } else {
+        parameterVariations[fullPath] = new Set([obj[key]]);
       }
     }
   }
-
-  return differences;
-}
-
-// Helper function for basic equality check (you might need a more robust one for complex objects)
-function isEqual(obj1, obj2) {
-  return JSON.stringify(obj1) === JSON.stringify(obj2); 
 }
